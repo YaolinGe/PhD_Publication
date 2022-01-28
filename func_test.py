6164ac3205a3758e14fd39bc3bc41330e5dbd308
Yaolin <44204509+YaolinGe@users.noreply.github.com>
2022-01-28 10:15:11 +0100

    Prepare_variogram

--- func_test.py
+++ func_test.py
@@ -3,3 +3,5 @@ import imgkit
 imgkit.from_file('/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Publication/Nidelva/fig/Visualisation/Myopic2D/myopic2d.html',
                  '/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Publication/Nidelva/fig/Visualisation/Myopic2D/test.jpg')
 
+
+
