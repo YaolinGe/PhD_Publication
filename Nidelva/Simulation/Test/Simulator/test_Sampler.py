733152e1ad2c67de40875c88aaceca11d0264827
Yaolin Ge <geyaolin@gmail.com>
2022-11-22 13:28:56 +0100

    clean_repo

--- Nidelva/Simulation/Test/Simulator/test_Sampler.py
+++
@@ -1,2 +0,0 @@
-print("")
-# TODO add test sampler
--- func_test.py
+++
@@ -1,8 +0,0 @@
-import imgkit
-
-imgkit.from_file('/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Publication/Nidelva/fig/Visualisation/Myopic2D/myopic2d.html',
-                 '/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Publication/Nidelva/fig/Visualisation/Myopic2D/test.jpg')
-
-
-
-
